cbeb43c0ac305383670f8fe6958748ff067b724b
Ololade0 <adesuyiololade@gmail.com>
2023-05-02 10:05:35 +0100

    One time password

--- DemoApp/src/main/java/com/demo/DemoApp/service/mail/EmailServiceImpl.java
+++ DemoApp/src/main/java/com/demo/DemoApp/service/mail/EmailServiceImpl.java
@@ -9,7 +9,7 @@ import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.mail.javamail.MimeMessageHelper;
 import org.springframework.stereotype.Service;
-
+//bbbbbbbbbbbbbbbbbbbbbbbbbbb
 import java.io.File;
 @Service
 public class EmailServiceImpl implements EmailService{
